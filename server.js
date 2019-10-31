a3c68821253e8d43cd0f6af95a82c979d813935e
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-31 00:03:50 -0700

    create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express            = require('express');
+const app                = express();
+const session            = require('express-session');
+
+const usersController    = require('./controllers/users');
+const articlesController = require('./controllers/articles');
+const photosController   = require('./controllers/photos');
+
+require('./db/db');
+
+app.use(session({
+    secret: "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: false}));
+
+app.use('/auth', usersController);
+app.use('/articles', articlesController);
+app.use('/photos', photosController);
+
+app.get('/',(req, res) => {
+    res.render('index')
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});

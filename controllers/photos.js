3f1a63ca666f324f59847492b9c3c59e5b0507e8
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-31 00:37:26 -0700

    add photos controller

---
+++ controllers/photos.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const Photo = require('../models/photos.js');
+const User = require('../models/users.js');
+
+
+
+module.exports = router;

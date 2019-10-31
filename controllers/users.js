151d3151b546c9d1796d07515497980d2626c57e
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-31 14:14:50 -0700

    add logout route

--- controllers/users.js
+++ controllers/users.js
@@ -58,5 +58,15 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+      if(err){
+        res.send(err);
+      } else {
+        res.redirect('/');
+      }
+    });
+});
+
 
 module.exports = router;
